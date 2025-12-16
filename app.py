ece100052bf4c52eb7c28618b03a9a937eb5acd5
rozer <shubhamsinha07084@gmail.com>
2025-12-16 12:45:26 +0530

    feat: Created web-app sidebar

--- app.py
+++ app.py
@@ -4,4 +4,43 @@ import os
 import tempfile
 
 st.set_page_config(page_title="AI CODE ASSISTANT", layout="wide")
-st.title("AI CODE ASSISTANT")
+
+# session state -->
+if "message" not in st.session_state:
+    st.session_state.message = []
+    
+if "mode" not in st.session_state:
+        st.session_state.mode = None
+    
+if "uploaded_texts" not in st.session_state:
+        st.session_state.uploaded_texts = {} # filename-> extracted text
+        
+st.title("🤖 AI CODE ASSISTANT")
+
+# sidebar upload -->
+st.sidebar.header("📁 Project Files")
+uploaded_files = st.sidebar.file_uploader("Upload your project files here", 
+    accept_multiple_files=True,
+    type=["pdf", "docx", "txt", "md", "csv", "json", "go", "js", "py", 
+          "tsx", "ts", "jsx", "java", "c", "cpp", "html", "css"
+          ]
+)
+
+# sidebar mode dropdown -->
+st.sidebar.header("⚙️ Select Mode")
+mode = st.sidebar.selectbox(
+    "Choose a mode for interaction (required):",
+    [
+        "General Guide",
+        "Debugger",
+        "Code Optimizer",
+        "Code Explainer",
+        "Project Builder",
+        "Review Your Code",
+        "Documentation Generator",
+        "Code Generator",
+    ],
+    index=0,
+)
+
+st.session_state.mode = mode
